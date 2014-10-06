433f6ae4b805a7cf587d74951270d39d4695fdfa
vshchutc <victoria.schutska@gmail.com>
2014-10-06 12:51:37 +0300

    functional completed

--- myScript.js
+++ myScript.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var totTask=0;
 	var totCompleted=0;
 	var totActive=0;
+	$('#mainCheck').prop('checked', false);
 	$('#textBox').keyup(function(e){
 		if(e.keyCode == 13){
 			var enteredString = $('#textBox').val();
@@ -47,8 +48,8 @@ $(document).ready(function() {
 			$("#itLeft").append(totActive);
 			$("#clear").empty();
 			$("#clear").append(totCompleted);
-			$(".chBx").parent().parent().find(".taskText").css( "text-decoration", "none" );
-			$(".chBx").parent().parent().find(".taskText").css( "color", "black" );
+			$(".chBx").parents().find(".taskText").css( "text-decoration", "none" );
+			$(".chBx").parents().find(".taskText").css( "color", "black" );
 			
 			
 		};
@@ -60,6 +61,7 @@ $(document).ready(function() {
 			if(check){
 			totActive--;
 			totCompleted++;
+			
 			$("#itLeft").empty();
 			$("#itLeft").append(totActive);
 			$("#clear").empty();
@@ -67,6 +69,7 @@ $(document).ready(function() {
 				$(this).parent().parent().find(".taskText").css( "text-decoration", "line-through" );
 				$(this).parent().parent().find(".taskText").css( "color", "grey" );
 			} else {
+				$('#mainCheck').prop('checked', false);
 				totActive++;
 				totCompleted--;
 				$("#itLeft").empty();
@@ -80,14 +83,51 @@ $(document).ready(function() {
 	});	
 	
 	$(document).on("click", ".dlBx",function() {
-		 $(this).parent().parent().remove();
-		 totTask=$(".chBx").length;
-		 $("#itLeft").empty();
-		 $("#itLeft").append(totActive);
-		 if(totTask == 0){
+		$(this).parent().parent().remove();
+		totTask=$(".chBx").length;
+		totCompleted=$(".chBx:checked").length;
+		$("#clear").empty();
+		$("#clear").append(totCompleted);
+		$("#itLeft").empty();
+		$("#itLeft").append(totActive);
+		if(totTask == 0){
 			$("#total").css( "visibility", "hidden" );	
 			}
 		});
-		
 	
+	$(document).on("click", "#all",function() {
+		$('#active,#completed').css("font-weight", "normal");
+		$('#all').css("font-weight", "bold");
+		$(".chBx:checked").parent().parent().css('display','table-row');
+		$(".chBx:not('checked')").parent().parent().css('display','table-row');
+		});
+		
+	$(document).on("click", "#active",function() {
+		$('#all,#completed').css("font-weight", "normal");
+		$('#active').css("font-weight", "bold");
+		$(".chBx:not('checked')").parent().parent().css('display', 'table-row');
+		$(".chBx:checked").parent().parent().css('display', 'none');
+		
+		
+		});
+		
+	$(document).on("click", "#completed",function() {
+		$('#all,#active').css("font-weight", "normal");
+		$('#completed').css("font-weight", "bold");
+		$(".chBx:not('checked')").parent().parent().css('display', 'none');
+		$(".chBx:checked").parent().parent().css('display', 'table-row');
+		});
+	
+	$(document).on("click", "#clearButton",function() {
+		$(".chBx:checked").parent().parent().remove();
+		$("#clear").empty();
+		var allCompleted=$(".chBx:checked").length;
+		totTask-=totCompleted;
+		totCompleted=0;
+		if(totTask == 0){
+			$("#total").css( "visibility", "hidden" );
+			$('#mainCheck').prop('checked', false);			
+			}
+		$("#clear").append(totCompleted);
+		});
 });
