cfcc161051de46ee4f30333e3a16845017313153
vshchutc <victoria.schutska@gmail.com>
2014-10-06 16:49:48 +0300

    partly refactoring

--- myScript.js
+++ myScript.js
@@ -1,133 +1,112 @@
 $(document).ready(function() {
-	var totTask=0;
-	var totCompleted=0;
-	var totActive=0;
+	var totalTask=0;
+	var totalCompletedTasks=0;
+	var totalActiveTask=0;
 	$('#mainCheck').prop('checked', false);
+	
+	function totalTaskNullHideBar(){
+		if(totalTask != 0){
+			$("#total").show("slow");
+		}else{$("#total").hide("slow");}
+	};
+	
+	totalTaskNullHideBar();
+	
 	$('#textBox').keyup(function(e){
 		if(e.keyCode == 13){
 			var enteredString = $('#textBox').val();
 			var trimedString = $.trim(enteredString);
-		
 			if(trimedString !== "") {
 				$('#taskKeeper').append('<div class="task"><div class="done"><input type="checkbox" name="test"'+ 
 				'class ="chBx"></div><div class="taskText">' + trimedString + 
 				'</div><div class = "del"><button class = "dlBx">X</button></div>');
-				totTask=$(".chBx").length;
-				totActive++;
-				$("#itLeft").empty();
-				$("#itLeft").append(totActive);
-				$("#total").css( "visibility", "visible" );
+				totalTask=$(".chBx").length;
+				totalActiveTask++;
+				$("#itLeft").html(totalActiveTask);
+				totalTaskNullHideBar();
 			}
-			$("#clear").empty();
-			$("#clear").append(totCompleted);
+			$("#clear").html(totalCompletedTasks);
 			$("#textBox").val("");
 		};
 	});
 	
 	$("#mainCheck").change(function(){
-		var a=this.checked;	
-		
-		if(a){
+		var testChecked=this.checked;	
+		if(testChecked){
 			$(".chBx").prop("checked", true);
-			totCompleted=totTask;
-			totActive=0;
-			$("#itLeft").empty();
-			$("#itLeft").append(totActive);
-			$("#clear").empty();
-			$("#clear").append(totCompleted);
-			
-			$(".chBx").parent().parent().find(".taskText").css( "text-decoration", "line-through" );
-			$(".chBx").parent().parent().find(".taskText").css( "color", "grey" );
-			
-			
-			} else {
+			totalCompletedTasks=totalTask;
+			totalActiveTask=0;
+			$("#itLeft").html(totalActiveTask);
+			$("#clear").html(totalCompletedTasks);
+			$(".chBx").parents(".task").find(".taskText").addClass( "checkedTask" );
+		} else {
 			$(".chBx").prop("checked", false);
-			totCompleted=0;
-			totActive=totTask;
-			$("#itLeft").empty();
-			$("#itLeft").append(totActive);
-			$("#clear").empty();
-			$("#clear").append(totCompleted);
-			$(".chBx").parents().find(".taskText").css( "text-decoration", "none" );
-			$(".chBx").parents().find(".taskText").css( "color", "black" );
-			
-			
+			totalCompletedTasks=0;
+			totalActiveTask=totalTask;
+			$("#itLeft").html(totalActiveTask);
+			$("#clear").html(totalCompletedTasks);
+			$('.chBx').parents(".task").find(".taskText").removeClass( "checkedTask" );
 		};
 	});
 	
-	
 	$(document).on("change", ".chBx",function() {
-			var check=this.checked;
-			if(check){
-			totActive--;
-			totCompleted++;
-			
-			$("#itLeft").empty();
-			$("#itLeft").append(totActive);
-			$("#clear").empty();
-			$("#clear").append(totCompleted);
-				$(this).parent().parent().find(".taskText").css( "text-decoration", "line-through" );
-				$(this).parent().parent().find(".taskText").css( "color", "grey" );
-			} else {
-				$('#mainCheck').prop('checked', false);
-				totActive++;
-				totCompleted--;
-				$("#itLeft").empty();
-				$("#itLeft").append(totActive);
-				$("#clear").empty();
-				$("#clear").append(totCompleted);
-				$(this).parent().parent().find(".taskText").css( "text-decoration", "none" );
-				$(this).parent().parent().find(".taskText").css( "color", "black" );
+		var isChecked=this.checked;
+		if(isChecked){
+		totalActiveTask--;
+		totalCompletedTasks++;
+		$("#itLeft").html(totalActiveTask);
+		$("#clear").html(totalCompletedTasks);
+		$(this).parents(".task").find(".taskText").addClass( "checkedTask" );
+		} else {
+			$('#mainCheck').prop('checked', false);
+			totalActiveTask++;
+			totalCompletedTasks--;
+			$("#itLeft").html(totalActiveTask);
+			$("#clear").html(totalCompletedTasks);
+			$(this).parents(".task").find(".taskText").removeClass( "checkedTask" );
 		};
-		
 	});	
 	
 	$(document).on("click", ".dlBx",function() {
 		$(this).parent().parent().remove();
-		totTask=$(".chBx").length;
-		totCompleted=$(".chBx:checked").length;
-		$("#clear").empty();
-		$("#clear").append(totCompleted);
-		$("#itLeft").empty();
-		$("#itLeft").append(totActive);
-		if(totTask == 0){
-			$("#total").css( "visibility", "hidden" );	
-			}
-		});
+		totalTask=$(".chBx").length;
+		totalCompletedTasks=$(".chBx:checked").length;
+		$("#clear").html(totalCompletedTasks);
+		$("#itLeft").html(totalActiveTask);
+		totalTaskNullHideBar()
+	});
 	
 	$(document).on("click", "#all",function() {
 		$('#active,#completed').css("font-weight", "normal");
 		$('#all').css("font-weight", "bold");
-		$(".chBx:checked").parent().parent().css('display','table-row');
-		$(".chBx:not('checked')").parent().parent().css('display','table-row');
+		$(".chBx").parents(".task").show();
 		});
 		
 	$(document).on("click", "#active",function() {
 		$('#all,#completed').css("font-weight", "normal");
 		$('#active').css("font-weight", "bold");
-		$(".chBx:not('checked')").parent().parent().css('display', 'table-row');
-		$(".chBx:checked").parent().parent().css('display', 'none');
-		
-		
-		});
+		$(".chBx:not('checked')").parents('.task').show();
+		$(".chBx:checked").parents('.task').hide();
+	});
 		
 	$(document).on("click", "#completed",function() {
-		$('#all,#active').css("font-weight", "normal");
-		$('#completed').css("font-weight", "bold");
-		$(".chBx:not('checked')").parent().parent().css('display', 'none');
-		$(".chBx:checked").parent().parent().css('display', 'table-row');
-		});
+		//$(document).change(".chBx", function(){
+			$('#all,#active').css("font-weight", "normal");
+			$('#completed').css("font-weight", "bold");
+			$(".chBx:not('checked')").parents('.task').hide();
+			$(".chBx:checked").parents('.task').show();
+		//});
+	});
 	
 	$(document).on("click", "#clearButton",function() {
-		$(".chBx:checked").parent().parent().remove();
-		$("#clear").empty();
+		$(".chBx:checked").parents(".task").remove();
 		var allCompleted=$(".chBx:checked").length;
-		totTask-=totCompleted;
-		totCompleted=0;
-		if(totTask == 0){
-			$("#total").css( "visibility", "hidden" );
+		totalTask-=totalCompletedTasks;
+		totalCompletedTasks=0;
+		totalTaskNullHideBar();
+		if(totalTask == 0){
 			$('#mainCheck').prop('checked', false);			
-			}
-		$("#clear").append(totCompleted);
-		});
+		};
+		$("#clear").append(totalCompletedTasks);
+	});
 });
